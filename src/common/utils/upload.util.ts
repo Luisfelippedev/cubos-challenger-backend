bdd3cc8c09438f17f4709190dbb4121419af73f7
Luisfelippedev <felippe.luisdev@gmail.com>
2025-08-11 17:43:51 -0300

    :hammer: refactor: Aumenta o tamanho máximo permitido para upload de imagens de 5MB para 10MB

--- src/common/utils/upload.util.ts
+++ src/common/utils/upload.util.ts
@@ -1,7 +1,7 @@
 import { Options } from 'multer';
 import { memoryStorage } from 'multer';
 
-export const MAX_IMAGE_SIZE_BYTES = 5 * 1024 * 1024;
+export const MAX_IMAGE_SIZE_BYTES = 10 * 1024 * 1024;
 export const ALLOWED_IMAGE_MIME_TYPES = [
   'image/jpeg',
   'image/png',
